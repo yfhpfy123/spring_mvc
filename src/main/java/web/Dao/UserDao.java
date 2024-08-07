e236c0f7b688f49f962456cf088fcbffb7c94cb6
yfhpfy123 <ivash_max@mail.ru>
2024-08-07 22:20:07 +0300

    2.3.1 test4

--- src/main/java/web/Dao/UserDao.java
+++ src/main/java/web/Dao/UserDao.java
@@ -1,4 +1,18 @@
 package web.Dao;
 
+import web.model.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    List<User> findAll();
+
+    User findOne(int id);
+
+    void save(User user);
+
+    void update(int id, User updatedUser);
+
+    void delete(int id);
 }
--- src/main/java/web/Dao/UserDaoImpl.java
+++ src/main/java/web/Dao/UserDaoImpl.java
@@ -1,4 +1,59 @@
 package web.Dao;
 
-public class UserDaoImpl {
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    private UserDaoImpl() {
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public User findOne(int id) {
+        TypedQuery<User> query = entityManager.createQuery("select u from User u where u.id = :id", User.class);
+        query.setParameter("id", id);
+        return query.getResultList().stream().findAny().orElse(null);
+    }
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void update(int id, User updatedUser) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
 }
