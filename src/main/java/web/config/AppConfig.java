5e61193b72f09f4ce971344330adafde5b92a7c7
yfhpfy123 <ivash_max@mail.ru>
2024-08-07 21:55:56 +0300

    2.3.1 test

---
+++ src/main/java/web/config/AppConfig.java
@@ -0,0 +1,67 @@
+package web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:app.properties")
+@ComponentScan("web")
+@EnableWebMvc
+@EnableTransactionManagement
+public class AppConfig {
+
+    private Properties property = new Properties();
+    private final Environment env;
+
+    @Autowired
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("db.driver")));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        property.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        property.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        property.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("web.model");
+        em.setJpaProperties(property);
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManager().getObject());
+        return transactionManager;
+    }
+}
