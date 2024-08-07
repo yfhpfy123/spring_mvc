c6db087bdebd8271414dd227c14dbeca1ea65813
yfhpfy123 <ivash_max@mail.ru>
2024-08-07 22:27:24 +0300

    2.3.1 test5

--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,4 +1,13 @@
 package web.service;
 
+import web.model.User;
+
 public interface UserService {
+    User findOne(int id);
+
+    void save(User user);
+
+    void update(int id, User updatedUser);
+
+    void delete(int id);
 }
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,4 +1,47 @@
 package web.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.Dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDaoImpl;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDaoImpl) {
+        this.userDaoImpl = userDaoImpl;
+    }
+
+    public List<User> findAll() {
+        return userDaoImpl.findAll();
+    }
+
+    @Override
+    public User findOne(int id) {
+        return userDaoImpl.findOne(id);
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        userDaoImpl.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(int id, User updatedUser) {
+        userDaoImpl.update(id, updatedUser);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+       userDaoImpl.delete(id);
+    }
 }
